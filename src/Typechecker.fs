dfe37f05fcea09bf13091a3146064cf5d58f8046
ailoaeicristina <ailoaeicristina@yahoo.com>
2023-03-30 19:39:44 +0200

    Added function subtyping

--- src/Typechecker.fs
+++ src/Typechecker.fs
@@ -152,6 +152,16 @@ let rec isSubtypeOf (env: TypingEnv) (t1: Type) (t2: Type): bool =
     | (t1, TVar(name)) ->
         // Expand the type variable; crash immediately if 'name' is not in 'env'
         isSubtypeOf env t1 (env.TypeVars.[name])
+    | (TFun(args1, ret1), TFun(args2, ret2)) ->
+        // The return type of the subtype function must be a subtype of the return type of the supertype function
+        if (not (isSubtypeOf env ret1 ret2)) then false
+        // Both function types expect the same number of arguments
+        elif args1.Length <> args2.Length then false
+        else 
+            // Each argument in the subtype function
+            // has a type that is a subtype of
+            // the argument found at the same position in the supertype function
+            List.forall2 (fun t1 t2 -> isSubtypeOf env t1 t2) args1 args2 
     | (TStruct(fields1), TStruct(fields2)) ->
         // A subtype struct must have at least the same fields of the supertype
         if fields1.Length < fields2.Length then false
