2eaa19dd4ebd62c6b07b9277f550efc82b5414a0
Troels Lund <trolund@gmail.com>
2023-08-17 10:11:53 +0200

    ok

--- run/Program.fs
+++ run/Program.fs
@@ -8,36 +8,36 @@ let runCode target watCode =
     let result = vm.RunWat(target, watCode)
     printfn "return: %A" result
 
-let first = 
-        // create a main function with the types 
-    let funcName = "start"
+// let first = 
+//         // create a main function with the types 
+//     let funcName = "start"
 
-    let body : Instructions =
-        [
-                Numeric (I32Const 41)
-                Numeric (I32Const 42)
-                Numeric (I32Add)
-                Control Return
-        ]
+//     let body : Instructions =
+//         [
+//                 Numeric (I32Const 41)
+//                 Numeric (I32Const 42)
+//                 Numeric (I32Add)
+//                 Control Return
+//         ]
 
-    let f: Function = Some(funcName), ([], [I32]), [], body
+//     let f: Function = Some(funcName), ([], [I32]), [], body
 
-    let _module = Module.create_module_instance
-                                            .add_function(f)
-                                            .add_export(funcName, FunctionType(funcName))
+//     let _module = Module.create_module_instance
+//                                             .add_function(f)
+//                                             .add_export(funcName, FunctionType(funcName))
 
-    // generate the wat code
-    let watCode = generate_module_code _module
-    // printfn "%s" watCode
+//     // generate the wat code
+//     let watCode = generate_module_code _module
+//     printfn "%s" watCode
 
-    // run the wat code with wasmtime 
-    runCode funcName watCode
+//     // run the wat code with wasmtime 
+//     runCode funcName watCode
 
 let sec = 
         // create a main function with the types 
     let funcName = "start"
 
-    let body : Instructions =
+    let body : List<Instruction> =
         [
                 Numeric (I32Const 41)
                 Numeric (I32Const 42)
@@ -45,11 +45,10 @@ let sec =
                 Control Return
         ]
 
-    let f: Function = Some(funcName), ([], [I32]), [], body
-    let f2: Function = Some(funcName + "2"), ([], [I32]), [], body
 
-    let _module = Modu().AddFunction(f, "function 1").AddExport(funcName, FunctionType(funcName)) + 
-                        Modu().AddFunction(f2).AddExport(funcName + "2", FunctionType(funcName + "2"))
+    let f: Function = Some(funcName), ([], [I32]), [], body
+
+    let _module = Modu().AddFunction(f, "function 1").AddExport(funcName, FunctionType(funcName))
                                  
     // generate the wat code
     let watCode = generate_module_code _module
@@ -59,11 +58,10 @@ let sec =
     // run the wat code with wasmtime 
     runCode funcName watCode
 
-[<EntryPoint>]
-let main argv = 
-    first |> ignore
+// [<EntryPoint>]
+// let main argv = 
+//    // first |> ignore
+//     //sec |> ignore
 
-
-
-    0 // return an integer exit code
+//     0 // return an integer exit code
 
