b03e4de60d58833f741e5c97bd29f5cc47bee9da
Troels Lund <trolund@gmail.com>
2023-08-30 12:00:22 +0200

    more

--- web/wasm-debugger/src/components/wasm-loader.tsx
+++ web/wasm-debugger/src/components/wasm-loader.tsx
@@ -1,53 +1,52 @@
-import { useState, useEffect } from 'react';
-import {fromEvent} from 'file-selector';
-
+import { useState, useEffect } from "react";
+import { fromEvent } from "file-selector";
+import { FileUploader } from "react-drag-drop-files";
 
 export const WasmLoader = () => {
   const [wasmResult, setWasmResult] = useState(null);
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
-  const setup = () => {
-    document.addEventListener('drop', async evt => {
-        evt.preventDefault();
-        const files = await fromEvent(evt);
-
-        const fileNames = files.map((f: any) => f.name);
-
-        setFile(fileNames[0]);
-        console.log(files);
-        });
-    }
-
-const selectFile = async () => {
-    // Open file picker
-    const handles = await (window as any).showOpenFilePicker({multiple: true});
-    // Get the files
-    const files = await fromEvent(handles);
-
-    // map to an array of file names
-    const fileNames = files.map((f: any) => f.name);
-
-    setFile(fileNames[0]);
-
-    console.log(files);
-    }
+  const fileTypes = ["wat", "wasm"];
 
   useEffect(() => {
-    setup()
     const loadWasm = async () => {
-      
-        fetch("test.wasm")
-        .then((response) => response.arrayBuffer())
-        .then((bytes) => WebAssembly.instantiate(bytes))
-        .then((results) => {
+      if (!file) {
+        return;
+      }
+
+        fetch(file.name)
+          .then((response) => response.arrayBuffer())
+          .then((bytes) => WebAssembly.instantiate(bytes))
+          .then((results) => {
             setWasmResult((results.instance.exports as any).main());
-        });
+          });
+
     };
     loadWasm();
-  }, []);
+  }, [file]);
 
-  return <>
-    <div><button onClick={selectFile}>Select file</button><input name='file-name' placeholder='file' value={file}></input></div>
-    <div>WebAssembly Result: {wasmResult}, {wasmResult == 0 ? <p>✅</p> : <p>❌</p>}</div>
-  </>;
+  const handleChange = (file: any) => {
+    console.log(file);
+    setFile(file);
+  };
+
+  return (
+    <>
+      <div>
+        <FileUploader
+          hoverTitle={"Drop here"}
+          required
+          multiple={false}
+          label={"File to run"}
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+        />
+      </div>
+      <div>
+        WebAssembly Result: {wasmResult},{" "}
+        {wasmResult == 0 ? <p>✅</p> : <p>❌</p>}
+      </div>
+    </>
+  );
 };
