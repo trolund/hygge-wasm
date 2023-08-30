1ac30dfba8c3fb9e041791d30669f51c733b5483
Troels Lund <trolund@gmail.com>
2023-08-30 11:29:46 +0200

    added web debugger project

---
+++ web/wasm-debugger/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { WasmLoader } from './components/wasm-loader';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to WebAssembly debugger</h2>
+        <p>Open the Chrome dev tools to debug the selected .wasm file</p>
+        <p>Set a break point</p>
+        <WasmLoader />
+        <button onClick={() => window.location.reload()}>
+          Start debugging
+        </button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
