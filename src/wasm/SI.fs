027cac8683ff6c409526ad9a841ce4ab5be7931d
Troels Lund <trolund@gmail.com>
2023-09-28 09:56:14 +0200

    clean up

--- src/wasm/SI.fs
+++ src/wasm/SI.fs
@@ -2,17 +2,15 @@
 module hyggec.SI
 open Wat.WFG
 
-let list = [
+/// all the imports from the host func that is provided by the system
+let si : Map<string, Import> =
+    Map.ofList [
     ("malloc", ("env", "malloc", FunctionType("malloc", Some([ (None, I32) ], [ I32 ]))))
     ("readInt", ("env", "readInt", FunctionType("readInt", Some(([], [ I32 ])))))
     ("readFloat", ("env", "readFloat", FunctionType("readFloat", Some(([], [ F32 ])))))
     ("writeInt", ("env", "writeInt", FunctionType("writeInt", Some(([ (None, I32) ], [])))))
     ("writeS", ("env", "writeS", FunctionType("writeS", Some(([ (None, I32); (None, I32) ], [])))))
     ]
-
-// all the imports from the WASI API
-let si : Map<string, Import> =
-    Map.ofList list
 
 /// provide function to get an import from the system interface 
 let getImport key =
